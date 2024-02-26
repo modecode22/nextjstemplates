3159f49bd62d22e80c59d9f9bf68ecd135095e04
moncef <epiccoder00@gmail.com>
2024-02-26 18:41:12 +0100

    added template type

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+type Template = {
+    title : string
+    description:string
+    tech:string[]
+    slug:string
+    tags:string[]
+    link:string
+    isFree:boolean
+    image:string
+    images?:string[]
+}
