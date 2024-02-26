5a3207302379a4536a73663cbdfe3714b89c7fba
moncef <epiccoder00@gmail.com>
2024-02-26 18:42:01 +0100

    started with the hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <section className="py-10">
+        <h1 className="text-pretty lg:text-6xl sm:text-5xl text-4xl lg:mb-8 font-bold mb-4 sm:mb-6">Free and Premium Nextjs templates</h1>
+        <p className="text-lg text-balance">A large set of nextjs tailwindcss typescript UI Templates</p>
+        <p className="text-lg text-pretty">For developers, indie hackers, SaaS companies, businesses, freelancers, and personal websites.</p>
+    </section>
+  )
+}
+
+export default Hero
