ee92572e7022c7ab260541b4421832f46b4ae227
moncef <epiccoder00@gmail.com>
2024-02-26 18:42:56 +0100

    started building templates data

---
+++ src/data/Templates.ts
@@ -0,0 +1,22 @@
+const tags = {
+    starter:'starter kit'
+}
+const tech = {
+    nextjs:'nextjs',
+    typescript: 'typescript',
+    tailwindcss: 'tailwindcss',
+    supabase:'supabase',
+    chadcn: 'chadcn',
+}
+export const Templates :Template[] = [
+    {
+        title:'Precedent',
+        description:'Precedent is an opinionated collection of components, hooks, and utilities for your Next.js project.',
+        image:'https://utfs.io/f/dc886f35-8691-477f-a283-7967f7f2f833-2zrus5.png',
+        isFree: true,
+        link:'https://precedent.dev/',
+        slug:'precedent',
+        tags: ['starter'],
+        tech:['nextjs' , ]
+    }
+]
